f4440dc6afcc0e22a77f42cc5dfb7b8ebdc7e8fa
razieh barzegar <razieh.brz.dev@gmail.com>
2025-07-09 10:52:21 +0330

    chore(types): define IBook and BackgroundStyle interfaces

---
+++ src/types/server.ts
@@ -0,0 +1,8 @@
+export interface IBook {
+  id: string;
+  imageUrl: string;
+  title: string;
+  author: string;
+  priceCents: number;
+  description: string;
+}
---
+++ src/types/styles.ts
@@ -0,0 +1,6 @@
+export interface BackgroundStyle {
+  backgroundImage: string;
+  backgroundRepeat: string;
+  backgroundSize: string;
+  backgroundPosition: string;
+}
