fcc4d3b0d91b2a136e84464506eb2e2b7cfb4452
razieh barzegar <razieh.brz.dev@gmail.com>
2025-07-09 11:02:29 +0330

    feat: add Home page component

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import Hero from "../../components/hero/Hero";
+import NewBooks from "../../components/newBooks/NewBooks";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <NewBooks />
+    </>
+  );
+}
+
+export default Home;
