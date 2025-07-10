fb44e713ebcd6d9a25753c027796b4a234c35bcb
razieh barzegar <razieh.brz.dev@gmail.com>
2025-07-10 17:17:58 +0330

    feat(router): add /cart route to application

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,24 @@ import Layout from "./components/layout/Layout";
 import Store from "./pages/store/Store";
 import BooksContextProvider from "./context/books/BooksContextProvider";
 import Product from "./pages/product/Product";
+import Cart from "./pages/cart/Cart";
+import ShoppingCartProvider from "./context/shoppingCart/ShoppingCartProvider";
 
 export function App() {
   return (
     <BooksContextProvider>
-      <div className="dark:text-white">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/books/:id" element={<Product />} />
-          </Routes>
-        </Layout>
-      </div>
+      <ShoppingCartProvider>
+        <div className="dark:text-white">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/store" element={<Store />} />
+              <Route path="/books/:id" element={<Product />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </Layout>
+        </div>
+      </ShoppingCartProvider>
     </BooksContextProvider>
   );
 }
