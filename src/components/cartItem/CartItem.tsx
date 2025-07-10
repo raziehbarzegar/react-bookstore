767c50e7375b3b14d1c90d7c0945eca66cad4220
razieh barzegar <razieh.brz.dev@gmail.com>
2025-07-10 17:09:36 +0330

    feat(cart): make CartItem dynamic using context methods and quantity

--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -1,30 +1,58 @@
-function CartItem() {
+import { useEffect, useState } from "react";
+import type { IBook } from "../../types/server";
+import { getBookById } from "../../services/api";
+import { useShoppingCartContext } from "../../hooks/context/useShoppingCartContext";
+
+interface ICartItem {
+  productId: number;
+  qty: number;
+}
+
+function CartItem({ productId, qty }: ICartItem) {
+  const [product, setProduct] = useState<IBook>();
+  const { handleIncreaseProductQty, handleDecreaseProductQty,handleRemoveProduct } =
+    useShoppingCartContext();
+
+  useEffect(() => {
+    getBookById(productId).then((data) => {
+      setProduct(data);
+    });
+  }, []);
+
   return (
-    <div className="shadow-md rounded-lg p-4 flex flex-col sm:flex-row sm:items-center gap-4 bg-white dark:bg-gray-800">
-      <div className="w-24 sm:w-32">
+    <div className="w-fit shadow-md dark:shadow-gray-700 rounded-lg py-8 px-16 sm:px-3 flex flex-col sm:flex-row sm:items-center gap-4">
+      <div className="w-24 sm:w-36">
         <img
-          src="/books/book1.jpg"
-          alt="Who's there?"
+          src={product?.imageUrl}
+          alt={product?.title}
           className="rounded-md w-full h-auto border border-gray-300"
         />
       </div>
 
       <div className="flex flex-col gap-3 w-full">
         <h3 className="text-xl font-semibold text-gray-800 dark:text-white">
-          Who's there?
+          {product?.title}
         </h3>
 
         <div className="flex items-center gap-2">
-          <button className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded-full text-sm">
+          <button
+            onClick={() => handleIncreaseProductQty(productId)}
+            className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded-full text-sm"
+          >
             +
           </button>
-          <span className="text-lg font-medium text-gray-700 dark:text-gray-200">4</span>
-          <button className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded-full text-sm">
+          <span className="text-lg font-medium text-gray-700 dark:text-gray-200">
+            {qty}
+          </span>
+          <button
+            onClick={() => handleDecreaseProductQty(productId)}
+            className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded-full text-sm"
+          >
             -
           </button>
         </div>
 
-        <button className="w-fit bg-red-500 hover:bg-red-600 text-white px-4 py-1 rounded-full text-sm mt-2">
+        <button onClick={()=>handleRemoveProduct(productId)} className="w-fit bg-red-500 hover:bg-red-600 text-white px-4 py-1 rounded-full text-sm mt-2">
           Remove
         </button>
       </div>
