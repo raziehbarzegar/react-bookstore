0d26d480ae6739bc25b869fe0cb12e8f32a88706
razieh barzegar <razieh.brz.dev@gmail.com>
2025-07-10 08:48:40 +0330

    feat(api): add getBookById function to fetch single book

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,3 +8,7 @@ export async function getAllBooks() {
   const { data } = await client("/books");
   return data;
 }
+export async function getBookById(id: number) {
+  const { data } = await client(`/books/${id}`);
+  return data;
+}
