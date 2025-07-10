9157789f335b140d414884225df784e56ad7086a
razieh barzegar <razieh.brz.dev@gmail.com>
2025-07-10 17:11:14 +0330

    feat(cart): create Cart page component

---
+++ src/pages/cart/Cart.tsx
@@ -0,0 +1,20 @@
+import CartItem from "../../components/cartItem/CartItem";
+import Container from "../../components/container/Container";
+import { useShoppingCartContext } from "../../hooks/context/useShoppingCartContext";
+
+function Cart() {
+  const { cartItems } = useShoppingCartContext();
+  return (
+    <div className="bg-white dark:bg-gray-800 py-20">
+      <Container>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-y-4 sm:gap-x-4 justify-center">
+          {cartItems.map((item) => (
+            <CartItem {...item} />
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Cart;
