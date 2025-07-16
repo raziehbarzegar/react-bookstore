d6aa86cc2c0046f7118c2e650e6ac1f421c1a057
razieh barzegar <razieh.brz.dev@gmail.com>
2025-07-16 10:29:54 +0330

    feat(loader): add spinner for loading

--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -4,6 +4,7 @@ import { getBookById } from "../../services/api";
 import { useShoppingCartContext } from "../../hooks/context/useShoppingCartContext";
 import { Link } from "react-router-dom";
 import Button from "../button/Button";
+import Spinner from "../spinner/Spinner";
 
 interface ICartItem {
   productId: string;
@@ -12,6 +13,7 @@ interface ICartItem {
 
 function CartItem({ productId, qty }: ICartItem) {
   const [product, setProduct] = useState<IBook>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const {
     handleIncreaseProductQty,
     handleDecreaseProductQty,
@@ -19,60 +21,71 @@ function CartItem({ productId, qty }: ICartItem) {
   } = useShoppingCartContext();
 
   useEffect(() => {
-    getBookById(productId).then((data) => {
-      setProduct(data);
-    });
+    setIsLoading(true);
+    getBookById(productId)
+      .then((data) => {
+        setProduct(data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   return (
     <div className="flex justify-center rounded-md dark:bg-gray-900">
-      <Link
-        to={`/books/${productId}`}
-        className="shadow-md rounded-lg py-8 px-16 sm:px-8 flex flex-col sm:flex-row sm:items-center gap-4"
-      >
-        <div className="w-24 sm:w-36">
-          <img
-            src={product?.imageUrl}
-            alt={product?.title}
-            className="rounded-md w-full h-auto border border-gray-300"
-          />
+      {isLoading ? (
+        <div className="flex items-center justify-center my-10">
+          <Spinner className="w-[50px] h-[50px]"/>
         </div>
-
-        <div className="flex flex-col gap-3 w-full">
-          <h3 className="text-xl font-semibold text-gray-800 dark:text-white">
-            {product?.title}
-          </h3>
-
-          <div className="flex items-center gap-2">
-            <Button
-              variant="primary"
-              onClick={() => handleIncreaseProductQty(productId)}
-              className="flex justify-center w-8 h-8 hover:!bg-primary/80 px-3 py-1 text-md font-bold"
-            >
-              +
-            </Button>
-            <span className="text-xl font-medium text-gray-700 dark:text-gray-200">
-              {qty}
-            </span>
-
-            <Button
-              variant="primary"
-              onClick={() => handleDecreaseProductQty(productId)}
-              className="flex justify-center w-8 h-8 hover:!bg-primary/80 px-3 py-1 text-md font-bold"
-            >
-              -
-            </Button>
+      ) : (
+        <Link
+          to={`/books/${productId}`}
+          className="shadow-md rounded-lg py-8 px-16 sm:px-8 flex flex-col sm:flex-row sm:items-center gap-4"
+        >
+          <div className="w-24 sm:w-36">
+            <img
+              src={product?.imageUrl}
+              alt={product?.title}
+              className="rounded-md w-full h-auto border border-gray-300"
+            />
           </div>
 
-          <Button
-            className="w-fit hover:!bg-red-600/80 px-4 py-1 text-md mt-2"
-            variant="danger"
-            onClick={() => handleRemoveProduct(productId)}
-          >
-            Remove
-          </Button>
-        </div>
-      </Link>
+          <div className="flex flex-col gap-3 w-full">
+            <h3 className="text-xl font-semibold text-gray-800 dark:text-white">
+              {product?.title}
+            </h3>
+
+            <div className="flex items-center gap-2">
+              <Button
+                variant="primary"
+                onClick={() => handleIncreaseProductQty(productId)}
+                className="flex justify-center w-8 h-8 hover:!bg-primary/80 px-3 py-1 text-md font-bold"
+              >
+                +
+              </Button>
+              <span className="text-xl font-medium text-gray-700 dark:text-gray-200">
+                {qty}
+              </span>
+
+              <Button
+                variant="primary"
+                onClick={() => handleDecreaseProductQty(productId)}
+                className="flex justify-center w-8 h-8 hover:!bg-primary/80 px-3 py-1 text-md font-bold"
+              >
+                -
+              </Button>
+            </div>
+
+            <Button
+              className="w-fit hover:!bg-red-600/80 px-4 py-1 text-md mt-2"
+              variant="danger"
+              onClick={() => handleRemoveProduct(productId)}
+            >
+              Remove
+            </Button>
+          </div>
+        </Link>
+      )}
     </div>
   );
 }
--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -5,9 +5,11 @@ import type { IBook } from "../../types/server";
 import { getBookById } from "../../services/api";
 import { useShoppingCartContext } from "../../hooks/context/useShoppingCartContext";
 import Button from "../../components/button/Button";
+import Spinner from "../../components/spinner/Spinner";
 
 function Product() {
   const [book, setBook] = useState<IBook | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const productId = useParams<{ id: string }>().id as string;
   const {
     handleIncreaseProductQty,
@@ -16,88 +18,102 @@ function Product() {
     handleRemoveProduct,
   } = useShoppingCartContext();
 
-
   useEffect(() => {
-    getBookById(productId).then((data) => {
-      setBook(data);
-    });
+    setIsLoading(true);
+    getBookById(productId)
+      .then((data) => {
+        setBook(data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, [productId]);
 
   return (
-    <div className="pt-36 dark:bg-gray-800 py-20">
-      <Container>
-        <div className="flex flex-col lg:flex-row gap-12 items-center justify-center">
-          <div>
-            <img
-              src={book?.imageUrl}
-              alt={book?.title}
-              className="w-64 md:w-80 rounded-lg shadow-lg border border-gray-300"
-            />
-          </div>
+    <>
+      {isLoading ? (
+        <div className="h-screen pt-32 flex items-center justify-center">
+          <Spinner className="w-[100px] h-[100px]" />
+        </div>
+      ) : (
+        <div className="pt-36 dark:bg-gray-800 py-20">
+          <Container>
+            <div className="flex flex-col lg:flex-row gap-12 items-center justify-center">
+              <div>
+                <img
+                  src={book?.imageUrl}
+                  alt={book?.title}
+                  className="w-64 md:w-80 rounded-lg shadow-lg border border-gray-300"
+                />
+              </div>
 
-          <div className="flex flex-col gap-4 text-gray-800 dark:text-gray-100">
-            <h1 className="text-4xl font-bold">{book?.title}</h1>
-            <h2 className="text-lg text-primary font-semibold">
-              by {book?.author}
-            </h2>
-            <p className="text-sm">Pages: {book?.pages}</p>
-            <p className="text-sm">
-              Price: $
-              {book?.priceCents != null
-                ? (book?.priceCents / 100).toFixed(2)
-                : ""}
-            </p>
-            <div className="">
-              {getProductQty(productId) === 0 ? (
-                <Button
-                  variant="primary"
-                  onClick={() => handleIncreaseProductQty(productId)}
-                  className="w-full hover:!bg-white hover:!text-primary/80 px-6 py-2 mt-4"
-                >
-                  Add To Cart
-                </Button>
-              ) : (
-                <>
-                  <div className="flex items-center gap-3">
+              <div className="flex flex-col gap-4 text-gray-800 dark:text-gray-100">
+                <h1 className="text-4xl font-bold">{book?.title}</h1>
+                <h2 className="text-lg text-primary font-semibold">
+                  by {book?.author}
+                </h2>
+                <p className="text-sm">Pages: {book?.pages}</p>
+                <p className="text-sm">
+                  Price: $
+                  {book?.priceCents != null
+                    ? (book?.priceCents / 100).toFixed(2)
+                    : ""}
+                </p>
+                <div className="">
+                  {getProductQty(productId) === 0 ? (
                     <Button
                       variant="primary"
                       onClick={() => handleIncreaseProductQty(productId)}
-                      className="flex justify-center font-bold w-8 h-8 hover:!bg-primary/80 px-3 py-1 text-md"
+                      className="w-full hover:!bg-white hover:!text-primary/80 px-6 py-2 mt-4"
                     >
-                      +
+                      Add To Cart
                     </Button>
-                    <span className="inline-block text-lg font-medium text-gray-700 dark:text-gray-200">
-                      {getProductQty(productId)}
-                    </span>
-                    <Button
-                      variant="primary"
-                      onClick={() => handleDecreaseProductQty(productId)}
-                      className="flex justify-center font-bold w-8 h-8 hover:!bg-primary/80 px-3 py-1 text-md"
-                    >
-                      -
-                    </Button>
-                  </div>
-                  <Button
-                    variant="danger"
-                    onClick={() => handleRemoveProduct(productId)}
-                    className="w-fit hover:!bg-red-600/80 px-4 py-1 text-sm mt-2"
-                  >
-                    Remove
-                  </Button>
-                </>
-              )}
+                  ) : (
+                    <>
+                      <div className="flex items-center gap-3">
+                        <Button
+                          variant="primary"
+                          onClick={() => handleIncreaseProductQty(productId)}
+                          className="flex justify-center font-bold w-8 h-8 hover:!bg-primary/80 px-3 py-1 text-md"
+                        >
+                          +
+                        </Button>
+                        <span className="inline-block text-lg font-medium text-gray-700 dark:text-gray-200">
+                          {getProductQty(productId)}
+                        </span>
+                        <Button
+                          variant="primary"
+                          onClick={() => handleDecreaseProductQty(productId)}
+                          className="flex justify-center font-bold w-8 h-8 hover:!bg-primary/80 px-3 py-1 text-md"
+                        >
+                          -
+                        </Button>
+                      </div>
+                      <Button
+                        variant="danger"
+                        onClick={() => handleRemoveProduct(productId)}
+                        className="w-fit hover:!bg-red-600/80 px-4 py-1 text-sm mt-2"
+                      >
+                        Remove
+                      </Button>
+                    </>
+                  )}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        <div className="mt-16 max-w-2xl mx-auto text-gray-700 dark:text-gray-300">
-          <h3 className="text-xl font-semibold mb-4 text-center">
-            Description
-          </h3>
-          <p className="text-justify leading-relaxed">{book?.description}</p>
+            <div className="mt-16 max-w-2xl mx-auto text-gray-700 dark:text-gray-300">
+              <h3 className="text-xl font-semibold mb-4 text-center">
+                Description
+              </h3>
+              <p className="text-justify leading-relaxed">
+                {book?.description}
+              </p>
+            </div>
+          </Container>
         </div>
-      </Container>
-    </div>
+      )}
+    </>
   );
 }
 
